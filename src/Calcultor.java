bad5bc80dcd8fc5581647a0690c3585efaedd563
Tobiasz <tobiaszkubiak31@gmail.com>
2020-03-04 14:59:26 +0100

    add implementation of calculator

---
+++ src/Calcultor.java
@@ -0,0 +1,12 @@
+public class Calcultor {
+
+  public static void main(String[] args) {
+    System.out.println(("W pierwszym branchu; 2+3=" + Policz(2, 3)));
+  }
+
+  private static String Policz(int i, int i1) {
+
+    return Integer.toString(i + i1);
+  }
+
+}
